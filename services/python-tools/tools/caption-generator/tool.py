044c8fd681b63633fdaa15d23807fa2c7660d42c
11PRIMUS <alokpal2803@gmail.com>
2026-05-11 01:21:09 +0530

    feat: caption-generator tool main

---
+++ services/python-tools/tools/caption-generator/tool.py
@@ -0,0 +1,10 @@
+MANIFEST = {
+    "id": "caption-generator",
+    "name": "Social Media Captions",
+    "description": "Describe your content and get platform-optimized captions with hashtags for Instagram, Twitter/X, LinkedIn, and more.",
+    "author": "Oxlo Team",
+    "version": "2.0.0",
+}
+
+async def run(data:dict)->dict:
+    return {"result": "", "metadata": {}}
